be7793d85710f5c682374a6f730f695b8ad0bf70
folder structure added <moinaktarshaikh651@gmail.com>
2026-04-20 17:14:02 +0530

    data fetching from tiingo: tiingo_client.py

--- backend/ingestion/tiingo_client.py
+++ backend/ingestion/tiingo_client.py
@@ -18,7 +18,23 @@ load_dotenv()
 TIINGO_API_KEY = os.getenv("TIINGO_API_KEY")
 sessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
+#fetching data from tiingo (async)
 async def getch_tiingo(symbol: str, start_date: datetime, end_date: datetime):
     url = f"https://api.tiingo.com/tiingo/daily/{symbol}/prices"
 
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Token {TIINGO_API_KEY}"
+    }
+
+    params = {
+        "startDate": start_date,
+        "endDate": end_date
+    }
+
+    async with httpx.AsyncClient() as client:
+        r = await client.get(url, headers=headers, params=params, timeout=20.0)
+        r.raise_for_status()
+        return r.json()
+    
     
