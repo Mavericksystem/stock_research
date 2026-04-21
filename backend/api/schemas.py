85cf8cabdf22e997c15b14d43ef87c892bd97a0a
folder structure added <moinaktarshaikh651@gmail.com>
2026-04-21 20:18:59 +0530

    generic respose wrapper: schemas.py

--- backend/api/schemas.py
+++ backend/api/schemas.py
@@ -2,4 +2,13 @@ from pydantic import BaseModel, ConfigDict
 from typing import Any, Optional, List
 from datetime import datetime
 
-class 
+#generic response wrapper
+class Meta(BaseModel):
+    timestamp: datetime = datetime.utcnow()
+    version: str = "v1"
+
+class APIResponse(BaseModel):
+    status: str = "success"
+    meta: Meta = Meta()
+
+# --- Domain Schema ===
