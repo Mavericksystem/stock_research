5c10a9840802f7cb66bdbc7eeed65dcbb592311b
folder structure added <moinaktarshaikh651@gmail.com>
2026-04-21 17:53:16 +0530

    fetch peices from db: signals.py

--- backend/processing/signals.py
+++ backend/processing/signals.py
@@ -13,4 +13,14 @@ SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
 async def compute_signal(symbol: str):
     """fetch prices, compute signals via pandas_ta, and upsert to database"""
-    async with SessionLocal
+    async with SessionLocal() as session:
+        #1. fetch prices from db
+        stmt = select(Prices).where(Price.symbol == symbol).order_by(Price.data.asc())
+        result = await session.execute(stmt)
+        prices = result.scalars().all()
+
+        if not prices:
+            print(f"No prices found for {symbil}")
+            return 
+        
+        #2. Convert to Dataframe
