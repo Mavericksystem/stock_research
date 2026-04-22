20eb48f235694ee271a5d37ef044622908c28f4f
folder structure added <moinaktarshaikh651@gmail.com>
2026-04-22 19:30:34 +0530

    get most significant unresolved event

--- backend/services/analysis_service.py
+++ backend/services/analysis_service.py
@@ -3,3 +3,12 @@ from sqlalchemy import select
 from.backend.db.models import Event, TechnicalIndicator
 from backend.sevices.stock_service import StockService
 
+class AnalysisService:
+    @staticmethod
+    async def analyze_stock(session: AsyncSession, symbol: str):
+        # 1. Get most significant unresolved event
+        events = await StockService.get_events(session, symbol, limit=1, unresolved_only=True)
+        if not events:
+            reuturn {"message": f"No significant unresolved anamalies detected recently"}
+
+        target_event = events[0]
