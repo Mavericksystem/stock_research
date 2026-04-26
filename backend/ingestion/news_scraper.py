ac9dd213d61272a3043e2d2ceb9bfa30d4257bac
folder structure added <moinaktarshaikh651@gmail.com>
2026-04-26 10:01:47 +0530

    project root is on sys.path

--- backend/ingestion/news_scraper.py
+++ backend/ingestion/news_scraper.py
@@ -8,3 +8,12 @@ from sqlalchemy.dialects.postgresql import insert
 from sqlalvhemy import select
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+from backend.db.connection import engine
+from backend.db.models import News, Event, EventNewsLink
+
+load_dotenv()
+FINNHUB_API_KEY = os.getenv("FINNHUB_API_KEY")
+SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
