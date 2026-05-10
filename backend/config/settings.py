bdec69b1e52424d6927f4225c0e5a54bc18d56d6
folder structure added <moinaktarshaikh651@gmail.com>
2026-05-10 21:06:33 +0530

    settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings
+from functools import lru_cache
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Market Explanation Engine"
@@ -9,8 +10,23 @@ class Settings(BaseSettings):
     FINNHUB_API_KEY: str
     TIINGO_API_KEY: str
 
+    # --Nvidia NIM--
+    NVIDIA_NIM_API_KEY: str
+    NVIDIA_NIM_BASE_URL: str = "https://integrate.api.nvidia.com/v1"
+    NVIDIA_NIM_MODEL: str = "nvidia/llama-3.3-nemotron-super-49b-v1"
+
+
+    # Agent config
+    AGENT_TEMPERATURE: float = 0.1
+    AGENT_MAX_TOKENS: int =1024
+    AGENT_TOP_p: float = 0.7
+
     class Config:
         env_file = ".env"
         case_sensitive = True
 
-settings = Settings()
+@lru_cache(maxsize=1)
+def get_settings() -> Settings:
+    return Settings()
+
+settings = get_settings()
