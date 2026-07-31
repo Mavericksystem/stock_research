090304946949e194d5b941d4aa57942b1488e023
Maverick7t <moinaktarshaikh651@gmail.com>
2026-07-31 13:00:00 +0530

    Update Tailwind configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  safelist: ["hidden"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
