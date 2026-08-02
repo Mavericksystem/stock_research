4d31ccf4b66836213c3a58d8c4d28f36d89a3b8f
Maverick7t <moinaktarshaikh651@gmail.com>
2026-08-02 10:00:00 +0530

    Remove legacy sidebar component

--- frontend/src/layouts/Sidebar.jsx
+++
@@ -1,5 +0,0 @@
-import EventsSidebar from "../features/anomalies/components/EventsSidebar";
-
-export default function Sidebar({ open, onToggle, onEventSelect }) {
-  return <EventsSidebar open={open} onToggle={onToggle} onEventSelect={onEventSelect} />;
-}
