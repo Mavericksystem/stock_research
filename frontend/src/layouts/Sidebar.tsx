d025edc7ef7bfaacd15b5389dbe43ec46581df64
Maverick7t <moinaktarshaikh651@gmail.com>
2026-08-16 15:34:08 +0530

    Update sidebar

--- frontend/src/layouts/Sidebar.tsx
+++ frontend/src/layouts/Sidebar.tsx
@@ -1,21 +1,21 @@
-import EventsSidebar from "../features/anomalies/components/EventsSidebar";
+// import EventsSidebar from "../features/anomalies/components/EventsSidebar";
 
-interface SidebarProps {
-  open: boolean;
-  onToggle: () => void;
-  onEventSelect: (question: string) => void;
-}
+// interface SidebarProps {
+//   open: boolean;
+//   onToggle: () => void;
+//   onEventSelect: (question: string) => void;
+// }
 
-export default function Sidebar({
-  open,
-  onToggle,
-  onEventSelect,
-}: SidebarProps) {
-  return (
-    <EventsSidebar
-      open={open}
-      onToggle={onToggle}
-      onEventSelect={onEventSelect}
-    />
-  );
-}
+// export default function Sidebar({
+//   open,
+//   onToggle,
+//   onEventSelect,
+// }: SidebarProps) {
+//   return (
+//     <EventsSidebar
+//       open={open}
+//       onToggle={onToggle}
+//       onEventSelect={onEventSelect}
+//     />
+//   );
+// }
